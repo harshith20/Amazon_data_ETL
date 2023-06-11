6f6982809b0ec116cad271a864c3e5907f075cbf
Harshith <73159496+harshith20@users.noreply.github.com>
2023-06-11 18:39:03 +0530

    Update amazon.py.py

--- amazon.py.py
+++ amazon.py.py
@@ -13,7 +13,7 @@ import numpy as np
 
 
 URL='https://www.amazon.in/s?k=mobiles&ref=nb_sb_noss_1'
-user_agent=['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Saf=ari/537.36 Edg/113.0.1774.42','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36']
+user_agent=[]
 
 
 # In[3]:
