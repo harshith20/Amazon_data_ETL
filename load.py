bc50c4243b0dc362ebe949c68f48f5daa19711ea
Harshith <73159496+harshith20@users.noreply.github.com>
2023-06-17 12:08:45 +0530

    Create load.py

---
+++ load.py
@@ -0,0 +1,14 @@
+from mage_ai.data_preparation.repo_manager import get_repo_path
+from mage_ai.io.bigquery import BigQuery
+from mage_ai.io.config import ConfigFileLoader
+from pandas import DataFrame
+from os import path
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data_to_big_query(df: DataFrame, **kwargs) -> None:
+    """
+    # use the code from mage ai to fill the required tokens 
